272227cd2a256626339e7ccc760dc7866cbe6b34
colaider <45285561+colaider@users.noreply.github.com>
2022-02-21 20:49:11 +0200

    d

---
+++ index.java
@@ -0,0 +1,24 @@
+
+ 
+ 
+ record Mechanical3dPuzzel(String garantie, String material, String typeOfPuzzel, int countOfElements){
+
+    public Mechanical3dPuzzel(String garantie, String material, String typeOfPuzzel, int countOfElements){
+        this.typeOfPuzzel = typeOfPuzzel;
+        this.garantie = garantie;
+        this.countOfElements = countOfElements;
+        this.material = material;
+    }
+   
+}
+
+
+
+
+
+class Main{
+    public static void main(String args[]){
+       
+
+    }
+}

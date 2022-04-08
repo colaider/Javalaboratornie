9a2a48d3176d234aa23db8d195210076dac440ed
colaider <45285561+colaider@users.noreply.github.com>
2022-04-08 20:43:46 +0200

    Add files via upload

---
+++ lab_3/tablewearPackage/Fork.java
@@ -0,0 +1,6 @@
+package tablewearPackage;
+public class Fork extends DisposibleTabelware{
+    public Fork(int setSize, double setPrice, String setMaterial, double setDateOfCreating){
+        super(setMaterial, setPrice, setDateOfCreating, setSize);
+    }  
+}
---
+++ lab_3/tablewearPackage/Spoon.java
@@ -0,0 +1,6 @@
+package tablewearPackage;
+public class Spoon extends DisposibleTabelware {
+    public Spoon(int setSize, double setPrice, String setMaterial, double setDateOfCreating){
+        super(setMaterial, setPrice, setDateOfCreating, setSize);
+    }  
+}
